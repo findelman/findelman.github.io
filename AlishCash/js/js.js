e6b4a93cfa01607bd9f20634ca88cffbdb75b577
findelman <Solomonkr@mail.ru>
2020-05-28 23:41:44 +0600

    parallax cards rework

--- AlishCash/js/js.js
+++ AlishCash/js/js.js
@@ -27,37 +27,11 @@ window.addEventListener('mousemove', function(e) {
 //end
 
 // cards parallax
-const cards = Array.from(document.querySelectorAll('.hover-card'));
-const props = {
-  perspective: '500px',
-  delta: 55, 
-  cardWidth: cards[0].clientWidth,
-  cardHeight: cards[0].clientHeight,
-}
-
-const midWidth = props.cardWidth / 4;
-const midHeight = props.cardHeight / 4;
-
-for (const card of cards) {
-  card.addEventListener('mousemove', mouseOverCard);
-  card.addEventListener('mouseleave', mouseLeftCard);
-}
-
-function mouseOverCard(e) {
-  const cursorX = e.pageX - this.offsetLeft; 
-  const cursorY = e.pageY - this.offsetTop;
-  const cursCenterX = midWidth - cursorX;
-  const cursCenterY = midHeight - cursorY;
-
-
-  Object.assign(this.style, {
-    transform: `perspective(${props.perspective}) rotateX(${cursCenterY / props.delta}deg) rotateY(${-cursCenterX / props.delta}deg)`
-  });
-  this.classList.remove('mouse-out');
-}
-function mouseLeftCard() {
-  this.classList.add('mouse-out');
-}
+VanillaTilt.init(document.querySelectorAll(".sk-left-box"), {
+  max: 12,
+  reverse:true,
+  perspective:    500, 
+});
 //end
 new WOW().init();
 // hamburger 
