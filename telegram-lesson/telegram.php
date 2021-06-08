ee026dade000bd301cbe4d3fcdb7c0a330fce4a8
findelman <Solomonkr@mail.ru>
2021-06-09 03:45:37 +0600

    123

---
+++ telegram-lesson/telegram.php
@@ -0,0 +1,22 @@
+<?php
+
+/* https://api.telegram.org/bot1881687961:AAHZej7Dc0FtXh902EuTstzcWMwgDZLdjl4/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "1881687961:AAHZej7Dc0FtXh902EuTstzcWMwgDZLdjl4";
+$chat_id = "935002005";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Сообщение' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+?>
